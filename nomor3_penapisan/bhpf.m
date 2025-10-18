9409c47f4ac06cca65cbef73fc8d78d468be0e87
Kizaaaa <13522059@std.stei.itb.ac.id>
2025-10-18 10:07:17 +0700

    feat: no 3

---
+++ nomor3_penapisan/bhpf.m
@@ -0,0 +1,58 @@
+function G2 = bhpf(image)
+    [M,N] = size(image);
+
+    % Step 1: Tentukan parameter padding, biasanya untuk citra f(x,y)
+    % berukuran M x N, parameter padding P dan Q biasanya P = 2M and Q = 2N.
+    P = 2*M;
+    Q = 2*N;
+
+    % Step 2: Bentuklah citra padding fp(x,y) berukuran P X Q dengan
+    % menambahkan pixel-pixel bernilai nol pada f(x, y).
+    image = im2double(image);
+    for i = 1:P
+         for j = 1:Q
+             if i <= M && j<= N
+                imagep(i,j) = image(i,j);
+             else
+                imagep(i,j) = 0;
+             end
+         end
+    end
+
+    % Step 3: Lakukan transformasi Fourier pada fp(x, y) dan tampilkan Fourier Spectrum
+    F = fftshift(fft2(imagep)); % move the origin of the transform to the center of the frequency rectangle
+    
+    % Step 4: Bangkitkan fungsi penapis H berukuran P x Q
+    % Penapis yang digunakan adalah Butterworth Highpass Filter (BHPF)
+    D0 = 50; % cut-off frequency
+    n = 2; % order of the filter
+    % Set up range of variables.
+    u = 0:(P-1);
+    v = 0:(Q-1);
+    % Compute the indices for use in meshgrid
+    idx = find(u > P/2);
+    u(idx) = u(idx) - P;
+    idy = find(v > Q/2);
+    v(idy) = v(idy) - Q;
+
+    % Compute the meshgrid arrays
+    [V, U] = meshgrid(v, u);
+    D = sqrt(U.^2 + V.^2);
+    
+    % Butterworth Highpass Filter: H = 1 / (1 + (D0/D)^(2n))
+    % Avoid division by zero
+    D(D == 0) = 0.01;
+    H = 1 ./ (1 + (D0./D).^(2*n));
+    H = fftshift(H);
+
+    % Step 5: Kalikan F dengan H
+    G = H.*F;
+    G1 = ifftshift(G);
+
+    % Step 6: Ambil bagian real dari inverse FFT of G:
+    G2 = real(ifft2(G1)); % apply the inverse, discrete Fourier transform
+
+    %Step 7: Potong bagian kiri atas sehingga menjadi berukuran citra semula
+    G2 = G2(1:M, 1:N); % Resize the image to undo padding
+    figure, imshow(G2); title('Butterworth Highpass Filter Output');
+end
---
+++ nomor3_penapisan/ghpf.m
@@ -0,0 +1,54 @@
+function G2 = ghpf(image)
+    [M,N] = size(image);
+
+    % Step 1: Tentukan parameter padding, biasanya untuk citra f(x,y)
+    % berukuran M x N, parameter padding P dan Q biasanya P = 2M and Q = 2N.
+    P = 2*M;
+    Q = 2*N;
+
+    % Step 2: Bentuklah citra padding fp(x,y) berukuran P X Q dengan
+    % menambahkan pixel-pixel bernilai nol pada f(x, y).
+    image = im2double(image);
+    for i = 1:P
+         for j = 1:Q
+             if i <= M && j<= N
+                imagep(i,j) = image(i,j);
+             else
+                imagep(i,j) = 0;
+             end
+         end
+    end
+
+    % Step 3: Lakukan transformasi Fourier pada fp(x, y) dan tampilkan Fourier Spectrum
+    F = fftshift(fft2(imagep)); % move the origin of the transform to the center of the frequency rectangle
+    
+    % Step 4: Bangkitkan fungsi penapis H berukuran P x Q
+    % Penapis yang digunakan adalah Gaussian High-pass Filter (GHPF)
+    D0 = 50; % cut-off frequency
+    % Set up range of variables.
+    u = 0:(P-1);
+    v = 0:(Q-1);
+    % Compute the indices for use in meshgrid
+    idx = find(u > P/2);
+    u(idx) = u(idx) - P;
+    idy = find(v > Q/2);
+    v(idy) = v(idy) - Q;
+
+    % Compute the meshgrid arrays
+    [V, U] = meshgrid(v, u);
+    D = sqrt(U.^2 + V.^2);
+    H = exp(-(D.^2)./(2*(D0^2)));
+    H = 1 - H;
+    H = fftshift(H);
+
+    % Step 5: Kalikan F dengan H
+    G = H.*F;
+    G1 = ifftshift(G);
+
+    % Step 6: Ambil bagian real dari inverse FFT of G:
+    G2 = real(ifft2(G1)); % apply the inverse, discrete Fourier transform
+
+    %Step 7: Potong bagian kiri atas sehingga menjadi berukuran citra semula
+    G2 = G2(1:M, 1:N); % Resize the image to undo padding
+    figure, imshow(G2); title('output image');
+end
